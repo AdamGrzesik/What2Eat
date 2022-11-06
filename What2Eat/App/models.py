6aef34ada9ba89260c0d06f3a3db4a94484f1ccf
Adam Grzesik <adam.grzesik45@gmail.com>
2022-11-06 13:09:56 +0100

    Adding Javascript, first attempt to create models, models can be modified through admin panel

--- What2Eat/App/models.py
+++ What2Eat/App/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    description = models.TextField(default='DUPA')
 
     def __str__(self):
         return self.name
@@ -10,7 +11,9 @@ class Recipe(models.Model):
 
 class Products(models.Model):
     name = models.CharField(max_length=100)
-    owned = models.BooleanField()
+    owned = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=0)
+    recipes = models.ManyToManyField(Recipe)
 
     def __str__(self):
         return self.name
--- What2Eat/App/views.py
+++ What2Eat/App/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
+from . import models
+
 
 # Deklarujemy funkcje które odpowiadają za renderowanie stron
 def index(response):
-    return render(response, 'App/base.html', {
-    })
+    return render(response, 'App/base.html')
 
 
 def homepage(response):
-    return render(response, 'App/home.html', {
-        'content': 'Strona dodatkowa',
-        'button_text': 'Podstawowa strona',
-    })
+    products = models.Products.objects.all()
+    context = {'products': products}
+    return render(response, 'App/home.html', context)
 
 
 def recipe(response):
-    return render(response, 'App/recipe.html', {
-    })
+    return render(response, 'App/recipe.html')
 
 
 def copyrights(response):
-    return render(response, 'App/copyrights.html', {
-    })
+    return render(response, 'App/copyrights.html')
