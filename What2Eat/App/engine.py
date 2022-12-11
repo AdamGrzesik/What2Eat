a3f0dfdb767233ba489a51ec8fde30c3f653d0b2
Adam Grzesik <adam.grzesik45@gmail.com>
2022-12-11 23:27:44 +0100

    Adding recipes

--- What2Eat/App/engine.py
+++ What2Eat/App/engine.py
@@ -3,18 +3,14 @@ import random
 
 def search():
     owned_products = models.Product.objects.filter(owned=True)
-    print(owned_products)
     recipes = models.Recipe.objects.filter(products__in=owned_products)
-    print(recipes)
     possible_recipes = []
     if not owned_products:
         return 'Air'
     else:
         for recipe in recipes:
             required_products = recipe.products.all()
-            print(required_products)
             common_elements = required_products.intersection(owned_products)
-            print(common_elements)
             if len(common_elements) == len(required_products):
                 possible_recipes.append(recipe)
             if not possible_recipes:
--- What2Eat/App/models.py
+++ What2Eat/App/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
-    description = models.TextField(default='nci')
+    description = models.TextField(default='Lorem ipsum')
     tags = models.JSONField(default=['tag1','tag2','tag3','tag4'])
     products = models.ManyToManyField(Product, related_name='products', blank=True)
 
